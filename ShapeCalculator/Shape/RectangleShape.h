f80ebf4e27d3649111fbcd48383f3fc99357c212
jckhoa <jckhoa@yahoo.com>
2019-10-07 09:29:50 +0200

    add geWidth(), getHeight(), and implementation for computePerimeter() and computeArea() to RectangleShape.h

--- ShapeCalculator/Shape/RectangleShape.h
+++ ShapeCalculator/Shape/RectangleShape.h
@@ -7,8 +7,19 @@
 class RectangleShape : public Shape2D {
 
 public:
+	virtual double getWidth() const = 0;
+	virtual double getHeight() const = 0;
 
 	virtual ~RectangleShape() = 0 {
 
+	}
+
+protected:
+	virtual double computePerimeter() const {
+		return (getWidth() + getHeight()) * 2;
+	}
+
+	virtual double computeArea() const {
+		return getWidth() * getHeight();
 	}
 };
